c0bc3331b915f9050bbbde059176c1a2d2e7e55f
Everty-Fernando <75959602+Everty-Fernando@users.noreply.github.com>
2025-07-16 20:00:40 -0300

    feat: initial design system settings such as colors, spacing, dimensions, themes

---
+++ shared/presentation/src/main/java/br/com/everty/shared/presentation/design_system/theme/Theme.kt
@@ -0,0 +1,48 @@
+package br.com.everty.shared.presentation.design_system.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import br.com.everty.shared.presentation.design_system.color.AppColors
+import br.com.everty.shared.presentation.design_system.color.AppDarkColors
+import br.com.everty.shared.presentation.design_system.color.AppLightColors
+
+private val DarkColorScheme = AppDarkColors
+
+private val LightColorScheme = AppLightColors
+
+val localAppThemeComposition = compositionLocalOf {
+    LightColorScheme
+}
+
+object AppTheme {
+    val colors: AppColors
+        @Composable get() = localAppThemeComposition.current
+    val typography: Typography
+        @Composable get() = AppTypography
+    val shapes: Shapes
+        @Composable get() = AppShapes
+}
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) AppDarkColors else AppLightColors
+    val colorScheme = if (darkTheme) DarkAppColorScheme else LightAppColorScheme
+
+    CompositionLocalProvider(localAppThemeComposition provides colors) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = AppTypography,
+            shapes = AppShapes,
+            content = content
+        )
+    }
+}
+
