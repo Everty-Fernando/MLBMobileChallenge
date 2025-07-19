77543da6a160270b737b15dc6f2142fed1e644b3
Everty-Fernando <75959602+Everty-Fernando@users.noreply.github.com>
2025-07-19 19:32:02 -0300

    feat: changing the loading layout of the product search screen

--- features/product/src/main/java/br/com/everty/product/presentation/product_search/components/ProductListLoadingContent.kt
+++ features/product/src/main/java/br/com/everty/product/presentation/product_search/components/ProductListLoadingContent.kt
@@ -1,72 +1,52 @@
 package br.com.everty.product.presentation.product_search.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.everty.shared.presentation.design_system.components.card.ProductCardSkeleton
 import br.com.everty.shared.presentation.design_system.components.loader.AppShimmerLoader
 import br.com.everty.shared.presentation.design_system.components.loader.AppShimmerLoaderRectangle
 import br.com.everty.shared.presentation.design_system.spacing.AppSpacing
 import br.com.everty.shared.presentation.design_system.theme.AppTheme
 
-
 @Composable
 fun ProductListLoadingContent() {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(
-            start = AppSpacing.base,
-            end = AppSpacing.base,
-            bottom = AppSpacing.xlarge
-        ),
+        contentPadding = PaddingValues(AppSpacing.base),
         verticalArrangement = Arrangement.spacedBy(AppSpacing.large),
         horizontalArrangement = Arrangement.spacedBy(AppSpacing.base),
         modifier = Modifier.fillMaxSize()
     ) {
-        items(6) {
+
+        item(span = { GridItemSpan(2) }) {
             AppShimmerLoader { brush ->
-                Column(
+                AppShimmerLoaderRectangle(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                        .clip(RoundedCornerShape(AppSpacing.small))
-                        .background(AppTheme.colors.background)
-                        .padding(AppSpacing.base)
-                ) {
-                    AppShimmerLoaderRectangle(
-                        modifier = Modifier
-                            .height(120.dp)
-                            .fillMaxWidth(),
-                        brush = brush
-                    )
-
-                    Spacer(modifier = Modifier.height(AppSpacing.base))
-
-                    repeat(2) {
-                        AppShimmerLoaderRectangle(
-                            modifier = Modifier
-                                .fillMaxWidth(if (it == 0) 1f else 0.6f)
-                                .height(12.dp),
-                            brush = brush
-                        )
-                        Spacer(modifier = Modifier.height(AppSpacing.tiny))
-                    }
-
-                    Spacer(modifier = Modifier.height(AppSpacing.small))
-
-                    AppShimmerLoaderRectangle(
-                        modifier = Modifier
-                            .width(80.dp)
-                            .height(14.dp),
-                        brush = brush
-                    )
-                }
+                        .height(19.dp)
+                        .fillMaxWidth(0.35f),
+                    brush = brush
+                )
             }
         }
+
+        items(6) {
+            AppShimmerLoader { brush ->
+                ProductCardSkeleton()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ProductListLoadingContentPreview() {
+    AppTheme {
+        ProductListLoadingContent()
     }
 }
