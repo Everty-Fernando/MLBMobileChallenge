b6bac4826f5bdb508e72e686e0f445e6e1ba64fc
Everty-Fernando <75959602+Everty-Fernando@users.noreply.github.com>
2025-07-20 00:17:36 -0300

    feat: add change in the color of the status bar

--- shared/presentation/src/main/java/br/com/everty/shared/presentation/design_system/theme/Theme.kt
+++ shared/presentation/src/main/java/br/com/everty/shared/presentation/design_system/theme/Theme.kt
@@ -6,10 +6,12 @@ import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.compositionLocalOf
 import br.com.everty.shared.presentation.design_system.color.AppColors
 import br.com.everty.shared.presentation.design_system.color.AppDarkColors
 import br.com.everty.shared.presentation.design_system.color.AppLightColors
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = AppDarkColors
 
@@ -35,6 +37,14 @@ fun AppTheme(
 ) {
     val colors = if (darkTheme) AppDarkColors else AppLightColors
     val colorScheme = if (darkTheme) DarkAppColorScheme else LightAppColorScheme
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setStatusBarColor(
+            color = colors.highlight,
+            darkIcons = false
+        )
+    }
 
     CompositionLocalProvider(localAppThemeComposition provides colors) {
         MaterialTheme(
