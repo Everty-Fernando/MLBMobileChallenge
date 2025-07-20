1c2552d6035b18011f4269cfbf3d717ecf7b047d
Everty-Fernando <75959602+Everty-Fernando@users.noreply.github.com>
2025-07-20 00:21:56 -0300

    feat: add BASE_DEV configuration in BuildConfig and update ApiClient to use it

--- shared/network/src/main/java/br/com/everty/shared/network/ApiClient.kt
+++ shared/network/src/main/java/br/com/everty/shared/network/ApiClient.kt
@@ -2,13 +2,11 @@ package br.com.everty.shared.network
 
 import retrofit2.Retrofit
 
-private const val BASE_DEV = "https://api.mercadolibre.com/"
-
 class ApiClient {
 
     val client: Retrofit by lazy {
         ApiBuilder(
-            url = BASE_DEV,
+            url = BuildConfig.BASE_DEV,
         ).client
     }
 }
