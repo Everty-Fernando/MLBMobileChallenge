0a1c48038087c1c56418b6a72e5a01b29765464c
Everty-Fernando <75959602+Everty-Fernando@users.noreply.github.com>
2025-07-20 21:49:48 -0300

    feat: update ProductModelUI to use default values for rating and ratingCount; add unit tests for ProductDetailsUIMapper and ProductUIMapper

--- features/product/src/main/java/br/com/everty/product/domain/model/ProductModelUI.kt
+++ features/product/src/main/java/br/com/everty/product/domain/model/ProductModelUI.kt
@@ -6,8 +6,8 @@ data class ProductModelUI(
     val title: String,
     val currentPrice: String,
     val originalPrice: String? = null,
-    val rating: Float? = null,
-    val ratingCount: Int? = null,
+    val rating: Float = 3.5f,
+    val ratingCount: Int = 1,
     val hasFreeShipping: Boolean = false,
     val isFavorite: Boolean = false
 )
